ee9d29ec3e74b52898b6d1f432bdf58e4f26b101
LakshyaRinwa <131532132+LakshyaRinwa@users.noreply.github.com>
2024-09-09 14:03:46 +0530

    emitting and listening event in diffrent files

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,2 +1,8 @@
 //import export questions practise krna hai isme
 
+const logger = require("./index");
+
+logger.logger.on("log", function(args){
+  console.log("Event Handled ", args)
+})
+logger.logger.log();
--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,5 +1,33 @@
-const EventEmiiter = require("events");  // returns eventEmitter class
+// const emit = require("./app");
 
-//class ka koi bhi varible bnayebge usey capls mein rkhna hai first letter
+// const EventEmiiter = require("events");  // returns eventEmitter class
 
-const emitter = new EventEmiiter();
+// //class ka koi bhi varible bnayebge usey capls mein rkhna hai first letter
+
+// const emitter = new EventEmiiter();
+
+// // emitter.addListener or emitter.on  for listening handling events
+
+
+
+// emitter.emit("logged", {username : "lakshya"});  //for raising an event
+
+// module.exports = {
+//     emitter,
+//     emit
+// }
+
+const EventEmitter = require("events");
+
+class Logger extends EventEmitter{
+
+    log(){
+        console.log("Event Emiited");
+        this.emit("log" , {id: 5030, username:"lakshya"});
+    }
+
+}
+
+const logger = new Logger();
+
+module.exports = {logger};
