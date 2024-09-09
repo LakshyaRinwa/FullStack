c62886f9985c8f61ade6081acabd67a547216d82
LakshyaRinwa <131532132+LakshyaRinwa@users.noreply.github.com>
2024-09-09 13:29:42 +0530

    eventEmitter

---
+++ Nodejs/index.js
@@ -0,0 +1,5 @@
+const EventEmiiter = require("events");  // returns eventEmitter class
+
+//class ka koi bhi varible bnayebge usey capls mein rkhna hai first letter
+
+const emitter = new EventEmiiter();
