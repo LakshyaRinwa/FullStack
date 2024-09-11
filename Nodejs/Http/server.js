cfc8f8d858a2974eb638669d70bc7c97053226b4
LakshyaRinwa <131532132+LakshyaRinwa@users.noreply.github.com>
2024-09-11 11:58:01 +0530

    sending files in response in http

--- Nodejs/Http/server.js
+++ Nodejs/Http/server.js
@@ -1,17 +1,23 @@
 const http = require("http"); // it will return us object or a class etc , here we let object returned
 
 const server = http.createServer((req,res)=>{
+
     res.setHeader("Content-Type" , "text/plain");
+
     //res.setHeader("Content-Type" , "text/html");
     // res.setHeader("Content-Type" , "Image/jpeg");
+    
     console.log("New Connection");
     res.write("Welcome to Lakshya Server");
+    
     // for send json data
     /*
     res.setHeader("Content-Type" , "application/json");
     res.write(JSON.stringify("this is json data"))
     */
+    
     res.end();
+    
 });
 
 server.listen(3000, (err)=>{   // it will tell where will be the req will come so we provide port
