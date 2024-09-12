c828208153b2c87d9507ebca687bb2d51f492970
LakshyaRinwa <131532132+LakshyaRinwa@users.noreply.github.com>
2024-09-12 12:46:04 +0530

    express practise

--- Express/server.js
+++ Express/server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const { dirname } = require("path");
+const app = express();
+
+//get post put patch delete update are functions of app.---
+// req.method == get to check req method is get or not
+
+app.get("/",(req,res)=>{
+   res.send("Hello Lakshya")
+})
+app.get("/index",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");  // send file absolute path pe kaam krta hai ye relative pe kaam nhi krta hai
+})
+app.listen(3000,(err)=>{
+    if(err)
+        console.log("error to run server ", err);
+    else
+    console.log("server is listening at port 3000");
+})
